a4ecd3557cf11639a9d6c139c7783a3af4e877ea
s4remi <s4remi@gmail.com>
2023-10-26 15:16:15 -0700

    creating the endpoint

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,9 @@ import bodyParser from "body-parser";
 export const router = express.Router();
 
 router.post("searchByIsbn", bodyParser.json(), async (req, res) => {
-  //const isbn=
+  const isbn = req.body.gotoresult;
+  const query = { ISBN: parseInt(isbn, 10) };
+  console.log(query);
 });
 
 export default router;
